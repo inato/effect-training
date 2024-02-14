3264d25075b3d9e1eb00f643d7300ade2ebbe59d
Beg <a.behaghel@inato.com>
2024-02-14 14:25:42 +0100

    🎨 Better structured tests

--- src/exo0 - Composing with pipe and flow/exo0.test.ts
+++ src/exo0 - Composing with pipe and flow/exo0.test.ts
@@ -1,6 +1,5 @@
+import { describe, expect, it } from "bun:test";
 import { isTestingSolution } from "../testUtils";
-import { expect, test, describe, it } from "bun:test";
-
 import * as exercise from "./exo0.exercise";
 import * as solution from "./exo0.solution";
 
@@ -8,39 +7,49 @@ const { isOddF, isOddP, next, next3 } = isTestingSolution()
   ? solution
   : exercise;
 
-test("should return true if the provided number is odd", () => {
-  const oddValue = 1;
+describe("exo0", () => {
+  describe("isOddP", () => {
+    it("should return true if the provided number is odd", () => {
+      const oddValue = 1;
 
-  expect(isOddP(oddValue)).toBe(true);
-});
+      expect(isOddP(oddValue)).toBe(true);
+    });
 
-test("should return false if the provided number is even", () => {
-  const oddValue = 2;
+    it("should return false if the provided number is even", () => {
+      const oddValue = 2;
 
-  expect(isOddP(oddValue)).toBe(false);
-});
+      expect(isOddP(oddValue)).toBe(false);
+    });
+  });
 
-test("should return true if the provided number is odd", () => {
-  const oddValue = 1;
+  describe("isOddF", () => {
+    it("should return true if the provided number is odd", () => {
+      const oddValue = 1;
 
-  expect(isOddF(oddValue)).toBe(true);
-});
+      expect(isOddF(oddValue)).toBe(true);
+    });
 
-test("should return false if the provided number is even", () => {
-  const oddValue = 2;
+    it("should return false if the provided number is even", () => {
+      const oddValue = 2;
 
-  expect(isOddF(oddValue)).toBe(false);
-});
+      expect(isOddF(oddValue)).toBe(false);
+    });
+  });
 
-test("should return the correct next element in the Collatz sequence", () => {
-  expect(next(4)).toBe(2);
-  expect(next(2)).toBe(1);
-  expect(next(1)).toBe(4);
-});
+  describe("next", () => {
+    it("should return the correct next element in the Collatz sequence", () => {
+      expect(next(4)).toBe(2);
+      expect(next(2)).toBe(1);
+      expect(next(1)).toBe(4);
+    });
+  });
 
-test("should return the correct element in the Collatz sequence 3 steps ahead", () => {
-  expect(next3(12)).toBe(10);
-  expect(next3(10)).toBe(8);
-  expect(next3(8)).toBe(1);
-  expect(next3(1)).toBe(1);
+  describe("next3", () => {
+    it("should return the correct element in the Collatz sequence 3 steps ahead", () => {
+      expect(next3(12)).toBe(10);
+      expect(next3(10)).toBe(8);
+      expect(next3(8)).toBe(1);
+      expect(next3(1)).toBe(1);
+    });
+  });
 });
